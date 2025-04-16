5be168f3d55949df66ed275356c4ede631994572
Mahesa <baskorocr@gmail.com>
2025-04-16 07:59:42 +0700

    Rename device components to follow Vue naming convention

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import ListDevices from "@/Pages/device/list.vue";
-import MonitoringDevice from "@/Pages/device/monitoring.vue";
+import ListDevices from "@/Pages/Device/List.vue";
+import MonitoringDevice from "@/Pages/Device/Monitoring.vue";
 
 const routes = [
     { path: "/devices", component: ListDevices },
